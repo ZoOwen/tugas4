77e592276d0761e9f225b5ac4814a1397da40a95
ZoOwen <raiffahmi2001@gmail.com>
2020-05-02 03:09:30 +0700

    update warna

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 type Sepeda struct {
-	nama string
-	ban  int
-	gigi int
+	warna string
+	ban   int
+	gigi  int
 }
 
 func (s *Sepeda) waktuTempuh(jarak float32) float32 {
@@ -17,18 +17,18 @@ func (s *Sepeda) waktuTempuh(jarak float32) float32 {
 
 func main() {
 	arraySepeda := make([]Sepeda, 5, 5)
-	arraySepeda[0] = Sepeda{nama: "bmx", ban: 2, gigi: 5}
-	arraySepeda[1] = Sepeda{nama: "sepeda gunung", ban: 2, gigi: 6}
-	arraySepeda[2] = Sepeda{nama: "ontel", ban: 2, gigi: 1}
-	arraySepeda[3] = Sepeda{nama: "sepeda anak", ban: 3, gigi: 1}
-	arraySepeda[4] = Sepeda{nama: "Sepeda santai", ban: 2, gigi: 3}
+	arraySepeda[0] = Sepeda{warna: "biru", ban: 2, gigi: 5}
+	arraySepeda[1] = Sepeda{warna: "hitam", ban: 2, gigi: 6}
+	arraySepeda[2] = Sepeda{warna: "putih", ban: 2, gigi: 1}
+	arraySepeda[3] = Sepeda{warna: "ungu", ban: 3, gigi: 1}
+	arraySepeda[4] = Sepeda{warna: "merah", ban: 2, gigi: 3}
 	fmt.Println(arraySepeda)
 
 	for _, value := range arraySepeda {
 
-		fmt.Println("jenis Sepeda = " + value.nama)
+		fmt.Println("Warna Sepeda = " + value.warna)
 		fmt.Println("jumlah ban   = ", value.ban)
 		fmt.Println("jumlah gigi  = ", value.gigi)
-		fmt.Println("waktu tempuh sepeda adalah sekitar ", value.waktuTempuh(20), "perjam")
+		fmt.Println("waktu tempuh sejauh 20 km adalah ", value.waktuTempuh(20), "menit ")
 	}
 }
